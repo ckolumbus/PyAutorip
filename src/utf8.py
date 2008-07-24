c05bebb8f7789f1c8154b8a8fd6da4d29f06d902
CKolumbus <ckolumbus@gmx.de>
2008-07-24 09:33:29 +0000

    rearrange

---
+++ src/utf8.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2003-2006 Edgewall Software
+# Copyright (C) 2003-2004 Jonas Borgström <jonas@edgewall.com>
+# Copyright (C) 2006 Matthew Good <trac@matt-good.net>
+# Copyright (C) 2005-2006 Christian Boos <cboos@neuf.fr>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.org/wiki/TracLicense.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://trac.edgewall.org/log/.
+#
+# Author: Jonas Borgström <jonas@edgewall.com>
+#         Matthew Good <trac@matt-good.net>
+#         Christian Boos <cboos@neuf.fr>
+
+import locale
+import os
+import sys
+from urllib import quote, quote_plus, unquote, urlencode
+
+
+CRLF = '\r\n'
+
+# -- Unicode
+
+def to_unicode(text, charset=None):
+    """Convert a `str` object to an `unicode` object.
+
+    If `charset` is given, we simply assume that encoding for the text,
+    but we'll use the "replace" mode so that the decoding will always
+    succeed.
+    If `charset` is ''not'' specified, we'll make some guesses, first
+    trying the UTF-8 encoding, then trying the locale preferred encoding,
+    in "replace" mode. This differs from the `unicode` builtin, which
+    by default uses the locale preferred encoding, in 'strict' mode,
+    and is therefore prompt to raise `UnicodeDecodeError`s.
+
+    Because of the "replace" mode, the original content might be altered.
+    If this is not what is wanted, one could map the original byte content
+    by using an encoding which maps each byte of the input to an unicode
+    character, e.g. by doing `unicode(text, 'iso-8859-1')`.
+    """
+    if not isinstance(text, str):
+        if isinstance(text, Exception):
+            # two possibilities for storing unicode strings in exception data:
+            try:
+                # custom __str__ method on the exception (e.g. PermissionError)
+                return unicode(text)
+            except UnicodeError:
+                # unicode arguments given to the exception (e.g. parse_date)
+                return ' '.join([to_unicode(arg) for arg in text.args])
+        return unicode(text)
+    if charset:
+        return unicode(text, charset, 'replace')
+    else:
+        try:
+            return unicode(text, 'utf-8')
+        except UnicodeError:
+            return unicode(text, locale.getpreferredencoding(), 'replace')
+
+def unicode_quote(value):
+    """A unicode aware version of urllib.quote"""
+    return quote(value.encode('utf-8'))
+
+def unicode_quote_plus(value):
+    """A unicode aware version of urllib.quote"""
+    return quote_plus(value.encode('utf-8'))
+
+def unicode_unquote(value):
+    """A unicode aware version of urllib.unquote.
+    
+    Take `str` value previously obtained by `unicode_quote`.
+    """
+    return unquote(value).decode('utf-8')
+
+def unicode_urlencode(params):
+    """A unicode aware version of urllib.urlencode"""
+    if isinstance(params, dict):
+        params = params.items()
+    return urlencode([(k, isinstance(v, unicode) and v.encode('utf-8') or v)
+                      for k, v in params])
+
+def to_utf8(text, charset='iso-8859-15'):
+    """Convert a string to UTF-8, assuming the encoding is either UTF-8, ISO
+    Latin-1, or as specified by the optional `charset` parameter.
+
+    ''Deprecated in 0.10. You should use `unicode` strings only.''
+    """
+    try:
+        # Do nothing if it's already utf-8
+        u = unicode(text, 'utf-8')
+        return text
+    except UnicodeError:
+        try:
+            # Use the user supplied charset if possible
+            u = unicode(text, charset)
+        except UnicodeError:
+            # This should always work
+            u = unicode(text, 'iso-8859-15')
+        return u.encode('utf-8')
+    except TypeError:
+        return text
+
